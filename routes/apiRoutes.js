9c93fd694004880ab9351c9c4c621491b0d161cc
Eric King <kingeric027@gmail.com>
2019-01-12 12:08:20 -0600

    renamed example to recipe and updated table in recipe.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,25 +2,38 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/recipes", function(req, res) {
+    db.Recipe.findAll({}).then(function(dbRecipe) {
+      res.json(dbRecipe);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new recipe
+  app.post("/api/recipes", function(req, res) {
+    db.Recipe.create({
+      title: req.body.title,
+      foodType: req.body.foodType,
+      ingredient1: req.body.ingredient1,
+      ingredient2: req.body.ingredient1,
+      ingredient3: req.body.ingredient1,
+      ingredient4: req.body.ingredient1,
+      ingredient5: req.body.ingredient1,
+      ingredient6: req.body.ingredient1,
+      ingredient7: req.body.ingredient1,
+      ingredient8: req.body.ingredient1,
+      ingredient9: req.body.ingredient1,
+      ingredient10: req.body.ingredient1
+    }).then(function(dbRecipe) {
+      res.json(dbRecipe);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
+  app.delete("/api/recipes/:id", function(req, res) {
+    db.Recipe.destroy({ where: { id: req.params.id } }).then(function(
+      dbRecipe
     ) {
-      res.json(dbExample);
+      res.json(dbRecipe);
     });
   });
 };
