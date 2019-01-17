6fff48560b0755751f66661a5673cae05790559f
Max <maxniederluecke@gmail.com>
2019-01-16 18:56:31 -0600

    update page and js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,11 +8,23 @@ module.exports = function(app) {
     });
   });
 
+  // Get recipes by food type
+  app.get("/api/recipes/:type", function(req, res) {
+    db.Recipe.findAll({
+      where: {
+        foodType: req.params.type
+      }
+    }).then(function(dbRecipe) {
+      res.json(dbRecipe);
+    });
+  });
+
   // Create a new recipe
   app.post("/api/recipes", function(req, res) {
     db.Recipe.create({
       title: req.body.title,
-      foodType: req.body.foodType
+      foodType: req.body.foodType,
+      description: req.body.description
     }).then(function(dbRecipe) {
       res.json(dbRecipe);
     });
